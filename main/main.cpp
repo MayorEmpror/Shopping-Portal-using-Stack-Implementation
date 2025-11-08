08054d240eade08085a910e8c49de1abe823261d
Hashir <hashirbutt2610@gmail.com>
2025-11-08 23:05:11 +0500

    feat: finalized the setup

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
-using namespace std;
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
 
-int main(){
-    cout<<"welcome to the project"<<endl;
-    return 0;
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("Qt GUI Window Example");
+    window.resize(400, 300);
+
+    QVBoxLayout *layout = new QVBoxLayout();
+
+    QLabel *label = new QLabel("Hello, Qt GUI!");
+    layout->addWidget(label);
+
+    QPushButton *button = new QPushButton("Click Me");
+    layout->addWidget(button);
+
+    QObject::connect(button, &QPushButton::clicked, [&]() {
+        label->setText("Button Clicked!");
+    });
+
+    window.setLayout(layout);
+    window.show();
+
+    return app.exec();
 }
