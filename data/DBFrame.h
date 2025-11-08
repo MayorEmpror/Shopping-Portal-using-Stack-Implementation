c891a240880473b7314d21dd2e27b3d1592f059c
Hashir <hashirbutt2610@gmail.com>
2025-11-08 21:02:36 +0500

    feat: folder restructured

---
+++ data/DBFrame.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+struct Product {
+    std::string name;
+    std::string category;
+    double price;
+    int stockQuantity;
+    std::string brand;
+    std::string model;
+    std::string color;
+    std::string dimensions;
+    double weight;
+    std::string material;
+    std::string warranty;
+    std::string releaseDate;
+    std::string originCountry;
+    std::string batteryLife;
+    double screenSize;
+    std::string processor;
+    int ramGB;
+    int storageGB;
+    std::string connectivity;
+    std::string features;
+};
