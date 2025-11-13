5ecb47a673a8ba588282103e97a1cc17086abf4d
Hashir <hashirbutt2610@gmail.com>
2025-11-13 21:41:58 +0500

    product object implemented

--- templates/products/main.h
+++ templates/products/main.h
@@ -13,8 +13,60 @@ class Products{
         private:
         string name;
         double price;
-        stack<Reviews> reviews;
-        
+        ArrayStack<Reviews> reviews;
+        string ImageURL;
+        string description;
+        string color;
+
+        Products(string name, double price, stack<Reviews> reviews, string ImageURL, string description, string color)
+        :name(name), price(price), reviews(reviews), ImageURL(ImageURL), description(description), color(color){}
+
+        Products(){
+                this->name = " ";
+                this->color = " ";
+                this->description = " ";
+                this->ImageURL = " ";
+                this->price = 0;       
+        }
+
+        Products(const Products& other){
+                 this->name = other.name;
+                 this->price  = other.price;
+                 this->reviews = other.reviews;
+                 this->ImageURL= other.ImageURL;
+                 this->color = other.color;
+                 this->description = other.description;
+        }
+
+        Products& operator =(const Products& other){
+                if(this != &other){
+                        this->name = other.name;
+                        this->price  = other.price;
+                        this->reviews = other.reviews;
+                        this->ImageURL= other.ImageURL;
+                        this->color = other.color;
+                        this->description = other.description;
+                }
+        }
+        string getname(){ return this->name;}
+        int getprice(){return this->price;}
+        string getdescription(){return this->description;}
+        string getcolor(){ return this->color;}
+        string getImageURL(){return this->ImageURL;}
+        ArrayStack<Reviews> getReivews(){ return this->reviews;}
+
+
+        void setname(string newname){  this->name = newname;}
+        void setprice(int newprice){ this->price = newprice;}
+        void setdescription(string newdesc){ this->description = newdesc;}
+        void setcolor(string newcolor){  this->color = newcolor;}
+        void setImageURL(string newIMAGE){ this->ImageURL = newIMAGE;}
+        void setReivews( ArrayStack<Reviews> newreviews){  this->reviews = newreviews;}
+
+
+        ~Products(){
+                delete[] &this->reviews;
+        }
 };
 
 #endif
